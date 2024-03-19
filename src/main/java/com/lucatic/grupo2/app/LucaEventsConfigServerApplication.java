b55ee375e87cf4001329d6c4de8b602335e5285e
jgp14 <jaimegarciaparada@gmail.com>
2024-03-19 16:37:17 +0100

    config server anotations

--- src/main/java/com/lucatic/grupo2/app/LucaEventsConfigServerApplication.java
+++ src/main/java/com/lucatic/grupo2/app/LucaEventsConfigServerApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Clase principal del ConfigServer
+ *
+ * @author BlueDevTeam
+ * @version 1.0.0
+ * @since 15-03-2024
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class LucaEventsConfigServerApplication {
 
+	/**
+	 * Punto de entrada al programa principal del ConfigServer
+	 * 
+	 * @param args Argumentos por línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(LucaEventsConfigServerApplication.class, args);
 	}
